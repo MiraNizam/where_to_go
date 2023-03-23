d0f51f55de608af369e8cd8381d6c1ba261eae8d
MiraNizam <82273561+MiraNizam@users.noreply.github.com>
2023-03-23 22:08:25 +0300

    Add frontend

--- where_to_go/where_to_go/settings.py
+++ where_to_go/where_to_go/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-4$)2n58x-ylx62kuree)(8fsbnql!jz*)st*alzhyz4c=-!(b2
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
+ALLOWED_HOSTS = ['127.0.0.1']
 
 
 # Application definition
@@ -117,6 +117,9 @@ USE_TZ = True
 
 STATIC_URL = "/static/"
 
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+]
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
