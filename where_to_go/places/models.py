71416986e3087402e92935a9511b09c90b1a90c5
MiraNizam <82273561+MiraNizam@users.noreply.github.com>
2023-03-25 12:35:19 +0300

    Update models.py

--- where_to_go/places/models.py
+++ where_to_go/places/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from tinymce import models as tinymce_models
 
-# Create your models here.
+
+class Place(models.Model):
+    title = models.CharField(max_length=200)
+    description_short = models.CharField(max_length=200)
+    description_long = models.TextField(blank=True)
+    coordinates = tinymce_models.HTMLField()
+    lng = models.FloatField()
+    lat = models.FloatField()
