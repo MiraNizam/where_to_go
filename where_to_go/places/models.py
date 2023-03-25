262c60809954c5babd60916e144a3fdcb9395edb
MiraNizam <82273561+MiraNizam@users.noreply.github.com>
2023-03-25 13:03:28 +0300

    Add  Place  model

--- where_to_go/places/models.py
+++ where_to_go/places/models.py
@@ -4,8 +4,11 @@ from tinymce import models as tinymce_models
 
 class Place(models.Model):
     title = models.CharField(max_length=200)
-    description_short = models.CharField(max_length=200)
+    description_short = models.TextField(blank=True)
     description_long = models.TextField(blank=True)
-    coordinates = tinymce_models.HTMLField()
+    coordinates = tinymce_models.HTMLField(blank=True)
     lng = models.FloatField()
     lat = models.FloatField()
+
+    def __str__(self):
+        return self.title
