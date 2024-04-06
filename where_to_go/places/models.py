d8a1b87805a5acdd58a9ff83ce2a4d38b191bc0b
MiraNizam <82273561+MiraNizam@users.noreply.github.com>
2024-04-06 12:08:21 +0300

    Add WYSIWYG-editor to model field log_description

--- where_to_go/places/models.py
+++ where_to_go/places/models.py
@@ -1,10 +1,11 @@
 from django.db import models
+from tinymce.models import HTMLField
 
 
 class Place(models.Model):
     title = models.CharField('Название места', max_length=200)
     description_short = models.TextField('Краткое описание места', blank=True)
-    description_long = models.TextField('Полное описание места', blank=True)
+    description_long = HTMLField('Полное описание места', blank=True)
     longitude = models.FloatField('Долгота')
     latitude = models.FloatField('Широта')
 
@@ -15,12 +16,10 @@ class Place(models.Model):
 class Image(models.Model):
     place = models.ForeignKey(Place, on_delete=models.CASCADE, verbose_name="Место", related_name="images")
     image = models.ImageField('Картинка')
-    position = models.IntegerField('Позиция картинки', default=0, db_index=True)
+    position = models.PositiveIntegerField('Позиция картинки', default=0, db_index=True, blank=False, null=False)
 
     class Meta:
-        ordering = [
-        'position'
-        ]
+        ordering = ['position', ]
 
     def __str__(self):
         return f"{self.position} {self.place}"
