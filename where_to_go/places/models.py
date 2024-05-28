3876f41c0c9f4de9910a2c92f3aa3eea9228c2ff
MiraNizam <82273561+MiraNizam@users.noreply.github.com>
2024-05-28 20:59:48 +0300

    Refactor: rename variables

--- where_to_go/places/models.py
+++ where_to_go/places/models.py
@@ -4,8 +4,8 @@ from tinymce.models import HTMLField
 
 class Place(models.Model):
     title = models.CharField("Название места", max_length=200)
-    description_short = models.TextField("Краткое описание места", blank=True)
-    description_long = HTMLField("Полное описание места", blank=True)
+    short_description = models.TextField("Краткое описание места", blank=True)
+    long_description = HTMLField("Полное описание места", blank=True)
     longitude = models.FloatField("Долгота")
     latitude = models.FloatField("Широта")
 
