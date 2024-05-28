bf578bcc3bc216ad6a6e3139d39e71d7f66a935c
MiraNizam <82273561+MiraNizam@users.noreply.github.com>
2024-05-28 21:15:20 +0300

    Update 0008_rename_description_long_place_long_description_and_more.py

--- where_to_go/places/migrations/0008_rename_description_long_place_long_description_and_more.py
+++ where_to_go/places/migrations/0008_rename_description_long_place_long_description_and_more.py
@@ -6,18 +6,18 @@ from django.db import migrations
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('places', '0007_alter_image_position_alter_place_description_long'),
+        ("places", "0007_alter_image_position_alter_place_description_long"),
     ]
 
     operations = [
         migrations.RenameField(
-            model_name='place',
-            old_name='description_long',
-            new_name='long_description',
+            model_name="place",
+            old_name="description_long",
+            new_name="long_description",
         ),
         migrations.RenameField(
-            model_name='place',
-            old_name='description_short',
-            new_name='short_description',
+            model_name="place",
+            old_name="description_short",
+            new_name="short_description",
         ),
     ]
